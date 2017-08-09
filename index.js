969ac2886ecc0ef2d028fd6562c91c3e645a546b
elektracodes <ebilali.simou@gmail.com>
2017-08-09 17:27:17 +0300

    4TY

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var osmosis = require('osmosis')
 var json2xls = require('json2xls');
+var _ = require('lodash');
 var fs = require('fs');
 var nextLink;
 var xls;
@@ -8,39 +9,44 @@ var array;
 function open_page(url) {
     console.log("Opening " + url);
     osmosis.get(url)
-        .find('#nav td:last a')
-        .set({
-            'nextLink': '@href'
-        })    
-        .find('.g')
-        .set({
-            'title':    '.r',
-            'url':      'cite',
-            'link':     '.r @href',
-            'text':     '.st'
-        })
-        .follow('.r @href')
-        .set({
-            'pageText': 'title'
-        })
-        .data(function(l) {
-		json.push(l);
+            .find('ul.entries div.entry > .wrapper')
+            .set({
+                'buiness': 'ul.entries div.entry div.description h2 a',
+                'tel': '.phone span',
+                'category': 'div.description h3 a',
+                'fulladdress': '.details .address'
+            })
+//            .find('ul.entries div.entry .actions> .site')
+//            .follow('@href')
+//            .find('body.subdomain table.contents')
+//            .set({
+//                'perioxi': 'span[itemprop=addressRegion]',
+//                't.k': 'span[itemprop=postalCode]'
+//            })
 
-            nextLink = l['nextLink'];
-         //   console.log(l);
 
-			})
-        .error(console.log)
-        .debug(console.log)
-        .done(function() {
-			console.log(json);
-					xls = json2xls(json);
-			write(xls)
-        //    open_page('https://www.google.co.in/' + nextLink);
-        })
+        //   .paginate('div.directory-catalog-paging a.next')
+            .data(function (l) {
+                json.push(l);
+
+                //nextLink = l['nextLink'];
+                //   console.log(l);
+             
+            })
+            .error(console.log)
+            .debug(console.log)
+            .done(function () {
+                   console.log(json);
+                var standardsList = _.uniqBy(json, 'tel');
+                console.log(standardsList);
+                xls = json2xls(standardsList);
+                write(xls)
+                //    open_page('https://www.google.co.in/' + nextLink);
+            })
 }
+
 function write(data) {
-	fs.writeFileSync('data.xlsx', data, 'binary');
+    fs.writeFileSync('nty4athens.xlsx', data, 'binary');
 }
-open_page('https://www.google.co.in/search?q=random+search');
+open_page('https://www.4ty.gr/index.php?l=el&p=business-categories&a=%CE%91%CE%B8%CE%B7%CE%BD%CE%B1');
 
